c8fefa471942dff01b0a6c18df1783d10ed5012b
Bryan Urquizo <bryanurquiso@gmail.com>
2024-01-11 21:04:14 -0500

    iniciando

---
+++ dia01/1_inicio.py
@@ -0,0 +1,48 @@
+nombre = 'Bryan'
+soltero = False
+
+# El comentario es muy importante para mantener nuestro codigo documentado
+print(nombre)
+
+numero1 = 10
+numero2 = 40
+
+# error -> no se puede sumar entre texto y un numero
+numero1 = '80'
+numero2 = '40'
+
+resultado = numero1 + numero2
+print(resultado)
+print(type(resultado))
+
+# para convertir de un tipo de dato a otro utilizo el tipo de dato que quiero convertir
+convertir_entero = int(numero1)
+
+print(type(convertir_entero))
+
+#  tipos de variables en python
+# int
+numero = 10
+
+# float
+calificacion = 10.4
+
+# boolean
+vacunado = False
+
+# string
+nombre = 'bryan'
+texto = '''
+Era una mañana de enero.
+Los pajaros cantaban...
+'''
+# si queremos texto largo con espacios y todo usamos triple comilla
+
+curso, grupo, mes, dia, hora = 'backend', 'G16', 'Enero', 11, 20
+
+# Forma de declarar variables, funciones, clases, metodos
+snake_case = 'snake_case'
+
+camelCase = 'camelCase'
+
+PascalCase = 'PascalCase'
---
+++ dia01/2_colecciones_de_datos.py
@@ -0,0 +1,18 @@
+# puedo agrupar varios valores en una variable
+
+# Listas
+# se puede modificar, ordenada por indices
+alumnos = ['victor', 'hiroito', 'marco', 'angel', 'bryan', 'samael', 'claudia']
+
+# las listas empiezan con la posicion 0
+print(alumnos[0])
+print(alumnos[4])
+
+# para saber el contenido (longitud) de datos
+# cuenta y no utiliza las posiciones
+print(len(alumnos))
+
+# si queremos recorrer la lista de derecha a izq utilizamos numeros negativos
+print(alumnos[-1])
+
+print(alumnos[len(alumnos)-1])
