e97509311bdc0bec03053536c39623e00f9aeb43
Bryan Urquizo <bryanurquiso@gmail.com>
2024-01-18 23:10:42 -0500

    flask uso de post en back y front

--- dia_2/1_productos_flask.py
+++ dia_2/1_productos_flask.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from uuid import uuid4
 from flask_cors import CORS
 
@@ -11,7 +11,7 @@ CORS(
     # desde que dominios se pueden acceder a mi api, si queremos que cualquier origen se conecte colocamos el *
     origins=['http://localhost:5500', 'http://127.0.0.1:5500'],
     #que headers(cabeceras) pueden enviar a mi api , *
-    allow_headers=['accept','authorization'] 
+    allow_headers=['accept','authorization', 'content-type'] 
     )
 
 productos = [
@@ -53,9 +53,31 @@ def gestionProductos():
 @app.route('/producto/<uuid:id>', methods=['GET'])
 def gestionProducto(id):
   print(id)
+  # tenemos una lista de productos en el cual en cada posicion tenemos un diccionario y una llave llamada id
+  # iteren esos productos y vean si existe el producto con determinado id
+  # si no existe entonces retornar un mensaje que dia 'Producto no existe' con un estado 404
+  # PISTA: hacer un for con if y else dentro de el
+  for producto in productos:
+    if producto['id'] == id:
+      return{
+        'content':producto
+      },200
   return{
-    'content':{}
-  }
+    'message':'El producto no existe'
+  },404
+
+@app.route('/producto', methods=['POST'])
+def crearProducto():
+  # convierte la data del body a un dictionary si el body es un JSON
+  data = request.get_json()
+  # antes de guardar 
+  data['id']=uuid4()
+
+  productos.append(data)
+  return{
+    'message':'Producto creado exitosamente',
+    'content': data
+  },201 #created
 
 if __name__ == '__main__':
   app.run(debug=True)
