cea521eb85acdbb34383fcb66d4a62c4809a69df
Bryan Urquizo <bryanurquiso@gmail.com>
2024-03-06 23:04:15 -0500

    avance

---
+++ src/conectores.js
@@ -0,0 +1,4 @@
+import { PrismaClient } from '@prisma/client'
+
+// export const conexion = new PrismaClient({datasources: 'mysql://usuario:password@host:3306/my_db'})
+export const conexion = new PrismaClient()
